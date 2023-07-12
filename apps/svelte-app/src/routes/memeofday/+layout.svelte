689053800ad66a85f647498b0d67b8618062b230
dmoriarty@fexco.ie <dmoriarty@fexco.ie>
2023-07-12 08:41:15 +0100

    basic SSR in Sveltekit

---
+++ apps/svelte-app/src/routes/memeofday/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+  import '../../app.css'
+</script>
+
+<div class="navbar bg-base-300">
+  <div class="flex-1">
+    <a class="btn btn-ghost normal-case text-xl">
+      <img src="/images/om.png" width="200" height="300">
+    </a>
+  </div>
+  <div class="flex-none">
+    <ul class="menu menu-horizontal px-1">
+      <li><a>Meme of the Day (SSR)</a></li>
+      <li><a>Meme Gallery (Static Rendering)</a></li>
+      <li><a>Meme Creator (Hydration)</a></li>
+      <li><a>Vote the Meme (SSG + Revalidation)</a></li>
+    </ul>
+  </div>
+</div>
+<slot />
--- apps/svelte-app/src/routes/memeofday/+page.svelte
+++ apps/svelte-app/src/routes/memeofday/+page.svelte
@@ -14,7 +14,7 @@
           tags: ["Doge", "Happy", "Funny"],
           created_at: "2023-01-01T00:00:00Z",
           updated_at: "2023-01-01T00:00:00Z",
-          creator: "John Doe"
+          creator: "John Doge"
         });
       }, 2000); // 2000 ms delay
     });
@@ -26,16 +26,23 @@
 </script>
 
 {#if memeOfTheDay === null}
-  <div>Loading...</div>
+  <span class="loading loading-dots loading-lg"></span>
 {:else}
   <div class="mx-auto px-6 sm:px-12 md:px-24 lg:px-32 max-w-7xl">
-    <h1 class="text-4xl font-bold text-center text-gray-700 my-8">Meme of the Day</h1>
+    <h1 class="text-4xl font-bold text-center text-white-700 my-8">Meme of the Day</h1>
     <div class="flex justify-center mb-8">
-      <img src={memeOfTheDay.url} alt={memeOfTheDay.title} width="500" height="500" />
+      <div class="card w-96 bg-base-100 shadow-xl">
+        <figure><img src={memeOfTheDay.url} alt={memeOfTheDay.title} width="500" height="500" /></figure>
+        <div class="card-body">
+          <h2 class="text-2xl font-semibold text-center text-yellow-600 mb-4">{memeOfTheDay.title}</h2>
+          <h3 class="card-title">{memeOfTheDay.description}</h3>
+          <p>Posted by: {memeOfTheDay.creator}</p>
+          <p>Category: {memeOfTheDay.category}</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">View</button>
+          </div>
+        </div>
+      </div>
     </div>
-    <h2 class="text-2xl font-semibold text-center text-gray-600 mb-4">{memeOfTheDay.title}</h2>
-    <p class="text-lg text-gray-700 mb-2">{memeOfTheDay.description}</p>
-    <p class="text-gray-500">Posted by: {memeOfTheDay.creator}</p>
-    <p class="text-gray-500 mb-8">Category: {memeOfTheDay.category}</p>
   </div>
 {/if}
